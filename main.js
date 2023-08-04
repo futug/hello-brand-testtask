def07e229f19403b52107add3ec07144fb91b981
futug <102650930+futug@users.noreply.github.com>
2023-08-04 19:07:51 +0600

    full  markup v1, without popup

--- main.js
+++ main.js
@@ -18,3 +18,49 @@ burger.addEventListener("click", () => {
 });
 
 // BURGER__MENU
+
+//MENU__OFFSET
+
+(function () {
+    const menuBlock = document.querySelector(".header-nav");
+    const header = document.querySelector(".header");
+    window.onscroll = () => {
+        if (window.pageYOffset > 50) {
+            menuBlock.classList.add("header-nav--active");
+            header.classList.add("header--active");
+        } else {
+            menuBlock.classList.remove("header-nav--active");
+            header.classList.remove("header--active");
+        }
+    };
+})();
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+for (let anchor of anchors) {
+    anchor.addEventListener("click", (e) => {
+        e.preventDefault();
+        const blockID = anchor.getAttribute("href");
+        document.querySelector("" + blockID).scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+        });
+    });
+}
+
+//MENU__OFFSET
+
+//SLIDER
+const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    direction: "horizontal",
+    centeredSlides: true,
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: ".button-next",
+        prevEl: ".button-prev",
+    },
+});
+
+//SLIDER
